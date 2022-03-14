11e4c8e37c627ba966130159fc88e768ae5327a1
JuanAsensioAyesa <jasenasen@gmail.com>
2022-03-14 11:31:22 +0100

    funciona el average

--- nanoVdb.cc
+++ nanoVdb.cc
@@ -12,8 +12,9 @@
 #include <openvdb/tools/ValueTransformer.h>
 #include <openvdb/Types.h>
 #include <iostream>
-float average_surrounding(const openvdb::FloatGrid::ValueOnCIter& iter){
-    auto vec = iter.getCoord().asVec3i();
+float average_surrounding(const openvdb::FloatGrid::ValueOnCIter& iter,openvdb::Coord coordenadas){
+    auto vec = coordenadas.asVec3i();
+    //auto vec = iter.getCoord().asVec3i();
     int incrementos[] = {-1,0,1};
     int len_incrementos = 3;
     float accum = 0;
@@ -56,11 +57,17 @@ struct Local {
         accessor.setValue(coords,iter.getValue());
         
     }
-    static inline void opAverage(const openvdb::FloatGrid::ValueOnCIter& iter,openvdb::FloatGrid::Accessor& accessor) {
+    static inline void opAverage(const  openvdb::FloatGrid::ValueOnCIter& iter,openvdb::FloatGrid::Accessor& accessor) {
         
         auto coords = iter.getCoord();
-        accessor.setValue(coords,average_surrounding(iter));
+        accessor.setValue(coords,average_surrounding(iter,coords));
        
+    }
+    static inline void opSet(const openvdb::FloatGrid::ValueOnCIter& iter,openvdb::FloatGrid::Accessor& accessor) {
+        
+        auto coords = iter.getCoord();
+        accessor.setValue(coords,0.0);
+
     }
 };
 template<class GridType>
@@ -101,8 +108,8 @@ void make_layer(GridType& grid, int size,int depth, openvdb::Coord& origin,float
     }
 }
 template<class GridType>
-void createSkin(GridType& grid,int size_lado,int profundidad_total){
-    openvdb::Coord coordenadas;
+void createSkin(GridType& grid,int size_lado,int profundidad_total,openvdb::Coord coordenadas){
+    
     
     //Epidermis = 0.2mm
     //  Stratum corneum
@@ -140,21 +147,55 @@ void createSkin(GridType& grid,int size_lado,int profundidad_total){
     //makeSkin(*grid,20,openvdb::Vec3f(1.5, 2, 3));
     //makeSphere(*grid, /*radius=*/50.0, /*center=*/openvdb::Vec3f(1.5, 2, 3));
     // Associate some metadata with the grid.
-    grid.insertMeta("radius", openvdb::FloatMetadata(50.0));
+    //grid.insertMeta("radius", openvdb::FloatMetadata(50.0));
     // Associate a scaling transform with the grid that sets the voxel size
     // to 0.5 units in world space.
     // grid.setTransform(
     //     openvdb::math::Transform::createLinearTransform(/*voxel size=*/0.01));
     // Identify the grid as a level set.
-    grid.setGridClass(openvdb::GRID_STAGGERED );
+    //grid.setGridClass(openvdb::GRID_STAGGERED );
     // Name the grid "LevelSetSphere".
-    grid.setName("LevelSetSphere");
+    //grid.setName("LevelSetSphere");
 }
 
+
+
 template<class GridType>
-void promediate(GridType& gridSource,GridType& gridDestiny){
+void promediate(GridType& gridSource,GridType& gridDestiny,int size_lado,int profundidad_total){
+    using ValueT = typename GridType::ValueType;
+    const ValueT outside = gridSource.background();
+    // Distance value for the constant region interior to the narrow band
+    // (by convention, the signed distance is negative in the interior of
+    // a level set)
+    
+    // Use the background value as the width in voxels of the narrow band.
+    // (The narrow band is centered on the surface of the sphere, which
+    // has distance 0.)
+    int padding = int(openvdb::math::RoundUp(openvdb::math::Abs(outside)));
+    // The bounding box of the narrow band is 2*dim voxels on a side.
+    int dim = int(size_lado + padding);
+    dim = size_lado;
     typename GridType::Accessor accessorSource = gridSource.getAccessor();
     typename GridType::Accessor accessorDestiny = gridDestiny.getAccessor();
+    int increment = 1;
+    openvdb::Coord origin(0,0,0);
+    int &i = origin[0], &j = origin[1], &k = origin[2];
+    int min_size = origin[0]-size_lado;
+    int min_depth = origin[1] - profundidad_total;
+    int min_size_2 = origin[2]-size_lado;
+    int k_0 = origin[2];
+    int i_0 = origin[0];
+    int j_0 = origin[1];
+    
+    for(k=k_0 ;k>min_size_2;k-=increment){
+        for(i=i_0;i > min_size;i-=increment){
+            for(j=j_0 ;j>min_depth;j-=increment){
+                accessorDestiny.setValue(origin, average_surrounding(gridSource.cbeginValueOn(),origin));
+                //std::cout<<origin<<std::endl;
+            }
+            //std::cout<<i<<std::endl;
+        }
+    }
     
 }
 /// @brief This examples depends on OpenVDB, NanoVDB and CUDA.
@@ -166,7 +207,8 @@ int main()
        openvdb::FloatGrid::create(/*background value=*/2.0);
         int size_lado = 250;
         int profundidad_total = 150;
-        createSkin(*grid,size_lado,profundidad_total);
+        openvdb::Coord coordenadas;
+        createSkin(*grid,size_lado,profundidad_total,coordenadas);
         
         // Converts the OpenVDB to NanoVDB and returns a GridHandle that uses CUDA for memory management.
         //auto handle = nanovdb::openToNanoVDB<nanovdb::CudaDeviceBuffer>(*grid);
@@ -190,19 +232,51 @@ int main()
 
         //cudaStreamDestroy(stream); // Destroy the CUDA stream
         //std::cout << "Value after scaling  = " << handle.grid<float>()->tree().getValue(nanovdb::Coord(101,0,0)) << std::endl;
+        int media_width = 20;//40 de ancho
+        int media_depth = 10;//20 de profundidad
+        openvdb::Coord coords_nuevas(-250/2+media_width,-profundidad_total/2+media_depth,-250/2+media_width);
+        make_layer(*grid,media_width*2,media_depth*2,coords_nuevas,10.0);
         openvdb::FloatGrid::Ptr outGrid = openvdb::FloatGrid::create(2.0);
-        
-        //openvdb::tools::transformValues(grid->cbeginValueOn(),*outGrid,Local::opCopia);
-        for(int i = 0 ;i<100;i++){
-            openvdb::tools::transformValues(grid->cbeginValueOn(),*outGrid,Local::opAverage);
+        openvdb::FloatGrid::Ptr readGrid;
+        openvdb::FloatGrid::Ptr writeGrid;
+        for(int i = 0 ;i<10;i++){
+            if(i%2==0){
+                readGrid = grid;
+                writeGrid = outGrid;
+            }else{
+                readGrid = outGrid;
+                outGrid = grid;
+            }
+            promediate(*readGrid,*writeGrid,size_lado,profundidad_total);
         }
-        //openvdb::tools::transformValues(grid->cbeginValueOn(),*outGrid,Local::opAverage);
         
         
+        // for(int i = 20 ;i<11;i++){
+        //     std::cout<<i<<std::endl;
+        //     // if(i%2 == 0 ){
+        //     //     readGrid = grid;
+        //     //     writeGrid = outGrid;
+        //     // }else{
+        //     //     readGrid = outGrid;
+        //     //     writeGrid = grid;
+        //     // }
+        //     // openvdb::tools::transformValues(readGrid->cbeginValueOn(),*writeGrid,Local::opAverage);
+        //     openvdb::tools::transformValues(grid->cbeginValueOn(),*outGrid,Local::opSet);
+        //     //openvdb::tools::transformValues(grid->cbeginValueOn(),*outGrid,Local::opAverage);
+        //     openvdb::tools::transformValues(outGrid->cbeginValueOn(),*grid,Local::opCopia);
+        //     //openvdb::tools::transformValues(grid->cbeginValueOn(),*outGrid,Local::opSet);
+
+        // }
+        // //openvdb::tools::transformValues(grid->cbeginValueOn(),*outGrid,Local::opAverage);
+        // int cont  = 0 ;
+        // std::cout<<grid->cbeginValueOn().summary()<<std::endl;
+        // //std::cout<<"Active "<<cont<<" Total:"<<size_lado*size_lado*profundidad_total<<std::endl;
+        //openvdb::Coord coords_nuevas_2(0,-profundidad_total-100,0);
+        //createSkin(*outGrid,size_lado,profundidad_total,coords_nuevas_2);
         openvdb::io::File file("mygrids.vdb");
         // Add the grid pointer to a container.
         openvdb::GridPtrVec grids;
-        grids.push_back(grid);
+        //grids.push_back(grid);
         grids.push_back(outGrid);
         // Write out the contents of the container.
         file.write(grids);
