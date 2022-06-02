83531a4290337806f43d44eef0f407aeb95239e3
JuanAsensioAyesa <jasenasen@gmail.com>
2022-06-02 19:59:01 +0200

    Experimento 2 tips

--- main.cc
+++ main.cc
@@ -256,12 +256,25 @@ int main(int argc,char * argv[]){
     tamanio_tumor = 1;
     createRectangle(accessor_1_Tip,esquina_izquierda,tamanio_tumor,1);
     createRectangle(accessor_2_Tip,esquina_izquierda,tamanio_tumor,1);
-    createRectangle(accessor_1_endothelial,esquina_izquierda,tamanio_tumor,1);
-    createRectangle(accessor_2_endothelial,esquina_izquierda,tamanio_tumor,1);
+    //createRectangle(accessor_1_endothelial,esquina_izquierda,tamanio_tumor,1);
+    //createRectangle(accessor_2_endothelial,esquina_izquierda,tamanio_tumor,1);
+
+
     
     createRectangle(accessor_1_Endothelial_discrete,esquina_izquierda,tamanio_tumor,1);
     createRectangle(accessor_2_Endothelial_discrete,esquina_izquierda,tamanio_tumor,1);
 
+    esquina_izquierda[0]+=50;
+    esquina_izquierda[1]+=2;
+    createRectangle(accessor_1_Tip,esquina_izquierda,tamanio_tumor,1);
+    createRectangle(accessor_2_Tip,esquina_izquierda,tamanio_tumor,1);
+    //createRectangle(accessor_1_endothelial,esquina_izquierda,tamanio_tumor,1);
+    //createRectangle(accessor_2_endothelial,esquina_izquierda,tamanio_tumor,1);
+
+
+    
+    createRectangle(accessor_1_Endothelial_discrete,esquina_izquierda,tamanio_tumor,1);
+    createRectangle(accessor_2_Endothelial_discrete,esquina_izquierda,tamanio_tumor,1);
     //createRectangle(accessor_2_endothelial,esquina_izquierda,tamanio_tumor,1);
     //createRectangle(accessor_1_MDE,esquina_izquierda,tamanio_tumor,10);
     //createRectangle(accessor_2_MDE,esquina_izquierda,tamanio_tumor,10);
